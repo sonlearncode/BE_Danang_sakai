55ed5e5d170fb2cc605692a32bfa69a29e626f20
VinhGiang731 <dinhvinhgiang345@gmail.com>
2025-11-18 10:25:33 +0700

    config app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const http = require('http');
-const { ENV } = require('./lib/env');
 const { connectDB } = require('../config/db.config');
 const mainRouter = require('./routes/index');
 const cors = require('../config/cors.config');
@@ -8,28 +6,30 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
-const PORT = ENV.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
+// Middlewares
 app.use(cors);
 app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
 
+// Routes
 app.use('/api/v1', mainRouter);
 
-app.use('/api/v1', (req, res) => {
+app.get('/api/v1', (req, res) => {
     res.status(200).json('DaNangScholar xin chào!');
-})
+});
 
+// Start Server
 const startServer = async () => {
     try {
         await connectDB();
 
-        http.createServer(app).listen(PORT, () => {
-            console.log('Server is running!');
-            console.log(`BASE URL: http://localhost:${PORT}/api/v1`);
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
         });
     } catch (error) {
-        console.log(`Server can't running: `, error);
+        console.error(`Server failed to start:`, error);
         process.exit(1);
     }
 };
